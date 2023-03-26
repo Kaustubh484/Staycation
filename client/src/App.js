19983cb5cb1f85b3873148856f52e4a9aaea46c7
Kaustubh484 <shahkaustubh6@gmail.com>
2023-03-26 13:49:30 +0530

    new

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
-
+import Login from "./pages/login/Login"
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/hotels" element={<List/>}/>
         <Route path="/hotels/:id" element={<Hotel/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react"
 import { AuthContext } from "../../context/AuthContext"
-
-const Login=()=>{
+import axios from "axios"
+ const Login=()=>{
 const[credentials,setCredentials]=useState({
     username:"",
     password:""
@@ -10,7 +10,7 @@ const {user,loading,error,dispatch} =useContext(AuthContext)
 const handleChange=(e)=>{
   setCredentials((prev)=>({
     ...prev,
-    [e.target.name]:e.target.name
+    [e.target.id]:e.target.value
   }
   ))
 }
@@ -19,20 +19,22 @@ const handleClick=async(e)=>{
 e.preventdefault();
 dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("/auth/login", credentials);
+      const res = await axios.post("http://localhost:5000/api/auth/login",
+       credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
       
     } catch (err) {
       dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
     }
-
+    console.log(user)
 }
 return(
     <div className="login">
-      <input type="text" placeholder="username" onChange={handleChange}className="username" />
-      <input type="text" placeholder="password" onChange={handleChange}className="password" />
+      <input type="text" id="username" placeholder="username" onChange={handleChange}className="username" />
+      <input type="text" id="password" placeholder="password" onChange={handleChange}className="password" />
        <button className="submit" onClick={handleClick}>Login</button>
     </div>
 )
 
 }
+export default Login
